394327e896baa706f912ce84788e56055cc8bd5d
HP <HP@DESKTOP-V3E85BB>
2022-12-31 00:44:09 +0530

    Add project files.

---
+++ LeaveManagement.Web/Data/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace LeaveManagement.Web.Data
+{
+    public partial class BaseEntity
+    {
+        public int Id { get; set; }
+        public int DateCreated { get; set; }
+        public int DateModified { get; set; }
+    }
+}
